ee6d3a461d3880dd6f843660780954546dbb3f79
Therasson <therassonkonan@gmail.com>
2018-09-04 09:17:22 +0000

    installation laravel collective et les vues

---
+++ resources/views/aidez_moi.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.core')
+
+@section('content')
+    <div style="background-color: #00519e; margin-bottom: 15px;" class="px-3 py-3 pt-md-5 pb-md-4">
+        <div class="container">
+            <!-- breadcrumbs -->
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="card-deck mb-3">
+            <div class="col-md-8">
+                <h3>Besoin d'aide? <small>Laissez nous un message</small></h3>
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label>Nom et prenoms</label>
+                        <input type="text" name="" class="form-control" placeholder="Votre nom ici">
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" name="" class="form-control" placeholder="Votre email ici">
+                    </div>
+                    <div class="form-group">
+                        <label>Objet</label>
+                        <input type="text" name="" class="form-control" placeholder="Votre objet ici">
+                    </div>
+                    <div class="form-group">
+                        <label>Votre message ici</label>
+                        <textarea name="" class="form-control" placeholder="Votre message ici"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" name="">
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,39 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'CoreController@index');
+
+Route::get('qui-sommes-nous', 'AboutUsController@index');
+
+Route::get('demande-d-intervention', 'DemandeDinterventionController@index');
+
+Route::get('achat-de-materiel', 'AchatController@index');
+
+Route::get('services-aux-particuliers', 'ServiceParticulierController@index');
+
+Route::get('services-aux-entreprises', 'ServiceEntrepriseController@index');
+
+Route::get('foire-aux-questions', 'FaqController@index');
+
+Route::get('base-de-connaissance', 'BaseConnaissanceController@index');
+
+
+Route::get('aidez-moi', 'AideController@index');
+
+Route:: get('en-savoir-plus', 'EnsavoirController@index');
+
+
+
+/******************************************************************************************/
+/******************** ROUTE ADMINISTRATION ************************************************/
+/******************************************************************************************/
+Route::get('admin', 'Admin\AdminController@index');
+
+Route::resource('admin/categorie', 'Admin\CategorieController');
+Route::get('admin/commande/new', 'Admin\CommandeController@new');
+Route::get('admin/commande/en-cours', 'Admin\CommandeController@enCours');
+Route::get('admin/commande/livree', 'Admin\CommandeController@livree');
+
+Route::resource('admin/produit', 'Admin\ProduitController');
+
+Route::resource('admin/utilisateur', 'Admin\UserController');
